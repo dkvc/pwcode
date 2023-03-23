d6f1641b2af3ec7ff8547137346cfc8b7aa988e3
Dhanush Kovi <99819848+dkvc@users.noreply.github.com>
2023-03-23 15:06:26 +0000

    feat: Autocreate config file without any user interaction
    Use async/await for look_for_changes()
    Make _path as private variable path (__path)

--- src/pwcode/utils/ConfigReader.py
+++ src/pwcode/utils/ConfigReader.py
@@ -2,26 +2,27 @@ from pathlib import Path
 
 import json
 import requests
+import sys
 
 
 class ConfigReader:
     """Class that creates, stores and looks for changes in config file"""
 
-    _path: Path
+    __path: Path
     papers: int
 
     def __init__(self):
-        self._path = Path.expanduser(Path("~/.pwconfig"))
+        self.__path = Path.expanduser(Path("~/.pwconfig"))
         self.papers = 0
 
     def __exists(self):
-        _path = self._path
-        if not (_path.exists() and _path.is_file):
+        __path = self.__path
+        if not (__path.exists() and __path.is_file):
             print("File doesn't exist.")
             return False
 
         try:
-            with open(_path, encoding="utf-8") as file:
+            with open(__path, encoding="utf-8") as file:
                 json.load(file)
         except json.JSONDecodeError:
             print("File is not a valid JSON file.")
@@ -29,14 +30,25 @@ class ConfigReader:
 
         return True
 
-    def look_for_changes(self):
+    def __autocreate(self):
+        config = {"papers": self.papers}
+        try:
+            with open(self._path, "w", encoding="utf-8") as file:
+                json.dump(config, file, skipkeys=True, indent=4)
+        except PermissionError:
+            print(
+                "Error: Permission Denied. Please check your home directory permissions."
+            )
+            sys.exit(1)
+
+    async def look_for_changes(self):
         if not self.__exists():
             self.__autocreate()
 
-        with open(self._path, encoding="utf-8") as file:
+        with open(self.__path, encoding="utf-8") as file:
             data = json.load(file)
 
-        papers = requests.get("https://paperswithcode.com/api/v1/papers/").json()
+        papers = await requests.get("https://paperswithcode.com/api/v1/papers/").json()
         self.papers = papers["count"]
         if value := self.papers > data["papers"]:
             # TODO: Temporary Link
