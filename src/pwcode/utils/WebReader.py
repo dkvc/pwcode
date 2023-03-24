3b507a945e16c74da0b12d3d52b976837dd026e9
Dhanush Kovi <99819848+dkvc@users.noreply.github.com>
2023-03-24 06:10:45 +0000

    feat: Add WebReader for reading latest papers from pwcode

---
+++ src/pwcode/utils/WebReader.py
@@ -0,0 +1,108 @@
+import json
+from logging.handlers import RotatingFileHandler
+from typing import List
+from api.Paper import Paper
+
+from bs4 import BeautifulSoup
+from pathlib import Path
+from tempfile import gettempdir
+
+import logging
+import os
+import requests
+import sys
+
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.INFO)
+
+handler = RotatingFileHandler('app.log', maxBytes=10000, backupCount=5)
+formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+handler.setFormatter(formatter)
+logger.addHandler(handler)
+
+class WebReader:
+    """Class that reads latest papers from pwcode"""
+    def __get_url(self, page: int):
+        __url = f"https://paperswithcode.com/latest?page={page}"
+        logger.info("URL selected: %s", __url)
+        return __url
+
+    def get_papers(self, pages: int):
+        if pages > 5:
+            pages = 5
+
+        latest = []
+
+        for page in range(1, pages + 1):
+            response = requests.get(self.__get_url(page))
+            content = response.content
+            logger.info("Content received for page %s", page)
+
+            soup = BeautifulSoup(content, "html.parser")
+            papers = soup.find_all("div", {"class": "col-lg-9 item-content"})
+            cache = self.ids(Path(os.path.join(gettempdir(), "pwcids.json")))
+
+            for paper in papers:
+                paper_id = paper.find("a")["href"].split("/")[-1]
+                if paper_id in cache:
+                    break
+                latest.append(paper_id)
+
+            logger.info("Number of latest papers: %s", len(latest))
+            self.__store_latest(latest, cache)
+
+        return latest
+
+    def ids(self, path: Path):
+        if not self.cache_exists(path):
+            self.__create_empty_cache(path)
+
+        try:
+            with open(path, "r", encoding="utf-8") as file:
+                data = file.read().splitlines()
+                logger.info("File %s is being read.", path)
+                return data
+        except PermissionError:
+            print("Error: Permission denied. Please check temp directory permissions.")
+            sys.exit(1)
+
+    def cache_exists(self, path: Path):
+        logger.info("File %s already exists", path)
+        return path.exists() and path.is_file()
+
+    def __create_empty_cache(self, path):
+        try:
+            with open(path, "w", encoding="utf-8") as file:
+                file.write("")
+            logger.info("File %s doesn't exist. Empty file is being created.", path)
+        except PermissionError:
+            print("Error: Permission denied. Please check temp directory permissions.")
+            sys.exit(1)
+
+    def __store_latest(self, latest: List[str], original: List[str]):
+        __path = Path(os.path.join(gettempdir(), "pwclatestids.json"))
+
+        try:
+            with open(__path, "w", encoding="utf-8") as file:
+                file.writelines([id + "\n" for id in latest])
+            logger.info("IDs written to pwclatestids.json")
+
+            __path = Path(os.path.join(gettempdir(), "pwclatest.json"))
+
+            data = {"papers": []}
+            for id in latest:
+                data["papers"].append(Paper(id).to_json())
+
+                with open(__path, "w", encoding="utf-8") as file:
+                    json.dump(data, file, skipkeys=True, indent=4)
+            logger.info("Latest Papers written to pwclatest.json")
+
+            __path = Path(os.path.join(gettempdir(), "pwcids.json"))
+
+            with open(__path, "w", encoding="utf-8") as file:
+                file.writelines([id + "\n" for id in original + latest])
+            logger.info("Latest Papers written to pwcids.json")
+            
+        except PermissionError:
+            print("Error: Permission denied. Please check temp directory permissions.")
+            sys.exit(1)
