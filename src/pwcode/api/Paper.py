e0ef635a4d5125397c27dab29607af72feaf710d
Dhanush Kovi <99819848+dkvc@users.noreply.github.com>
2023-03-24 06:07:43 +0000

    In Paper.py feat: Remove pendulum dependency feat: Add to_json() function chore: Add Logging

--- src/pwcode/api/Paper.py
+++ src/pwcode/api/Paper.py
@@ -1,10 +1,17 @@
-import pendulum
+import logging
+from logging.handlers import RotatingFileHandler
 import requests
 
-from dataclasses import dataclass
+from dataclasses import dataclass, field
 from typing import List, Union
-from pendulum import Date
 
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.INFO)
+
+handler = RotatingFileHandler('app.log', maxBytes=10485760, backupCount=5)
+formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+handler.setFormatter(formatter)
+logger.addHandler(handler)
 
 @dataclass
 class Paper:
@@ -12,8 +19,8 @@ class Paper:
 
     id: str
     title: Union[str, None] = None
-    published: Date = pendulum.now().date()
-    authors: List[str] = list()
+    published: Union[str, None] = None
+    authors: List[str] = field(default_factory=lambda: [])
     abstract: Union[str, None] = None
 
     pwc_url: Union[str, None] = None
@@ -23,17 +30,19 @@ class Paper:
 
     def __post_init__(self):
         """If id is given, call the api and get information about the paper using id."""
-        self.pwc_url = f"https://paperswithcode.com/paper/{id}"
+        self.pwc_url = f"https://paperswithcode.com/paper/{self.id}"
 
-        api_url = f"https://paperswithcode.com/api/v1/papers/{id}"
+        api_url = f"https://paperswithcode.com/api/v1/papers/{self.id}"
         paper = requests.get(api_url).json()
         paper_git = requests.get(api_url + "/repositories/").json()
-        paper_git = paper_git["results"][0]
+        logger.info("URLs called successfully.")
+
+        paper_git = paper_git["results"][0] if "results" in paper_git.keys() else {}
         paper_keys, paper_git_keys = paper.keys(), paper_git.keys()
 
         self.title = paper["title"] if "title" in paper_keys else None
         self.published = (
-            pendulum.parse(paper["published"]) if "published" in paper_keys else None
+            paper["published"] if "published" in paper_keys else None
         )
 
         self.authors = paper["authors"] if "authors" in paper_keys else None
@@ -45,3 +54,18 @@ class Paper:
         self.framework = (
             paper_git["framework"] if "framework" in paper_git_keys else None
         )
+        
+        logger.info("All fields of Paper %s are initialized.", self.id)
+
+    def to_json(self):
+        return {
+            "id": self.id,
+            "title": self.title,
+            "published": self.published,
+            "authors": self.authors,
+            "abstract": self.abstract,
+            "pwc_url": self.pwc_url,
+            "git_url": self.git_url,
+            "pdf_url": self.pdf_url,
+            "framework": self.framework,
+        }
