dkvc/pwcode

commit 86ee955737d7d97b141ea20a2f57120e1f6afcd4

deploy: Fix paths for deployment

src/pwcode/utils/ConfigReader.py
@@ -26,11 +26,7 @@ class ConfigReader:
 
     def __init__(self):
         self.papers = 0
-
-        if "XDG_CONFIG_HOME" in os.environ:
-            self.__path = Path(os.environ["XDG_CONFIG_HOME"]).joinpath(".pwconfig")
-        else:
-            self.__path = Path.expanduser(Path("~/.config/.pwconfig"))
+        self.__path = '.pwconfig'
 
     def __exists(self):
         __path = self.__path

src/pwcode/main.py
@@ -4,9 +4,7 @@ import os
 
 from fastapi import FastAPI, Response
 from json import load as jsonload
-from pathlib import Path
 from random import randint
-from tempfile import gettempdir
 
 from pwcode.api.Paper import Paper
 from pwcode.utils.ConfigReader import ConfigReader
@@ -54,8 +52,7 @@ def latest():
     else:
         filename = "pwclatest.json"
 
-    __path = Path(os.path.join(gettempdir(), filename))
-    with open(__path, "r", encoding="utf-8") as file:
+    with open(filename, "r", encoding="utf-8") as file:
         data = jsonload(file)
 
     logger.info("JSON file %s loaded successfully.", filename)

src/pwcode/utils/WebReader.py
@@ -7,7 +7,6 @@ import sys
 
 from bs4 import BeautifulSoup
 from pathlib import Path
-from tempfile import gettempdir
 from typing import List
 
 from pwcode.api.Paper import Paper
@@ -37,7 +36,7 @@ class WebReader:
             pages = 5
 
         latest = []
-        cache = self.ids(Path(os.path.join(gettempdir(), "pwcids.json")))
+        cache = self.ids(Path(os.path.join(Path.cwd(), "pwcids.json")))
 
         for page in range(1, pages + 1):
             response = requests.get(self.__get_url(page))
@@ -85,19 +84,19 @@ class WebReader:
             sys.exit(1)
 
     def __store_latest(self, latest: List[str], original: List[str]):
-        __path = Path(os.path.join(gettempdir(), "pwclatestids.json"))
+        __path = Path(os.path.join(Path.cwd(), "pwclatestids.json"))
 
         try:
             with open(__path, "w", encoding="utf-8") as file:
                 file.writelines([id + "\n" for id in latest])
             logger.info("IDs written to pwclatestids.json")
 
-            __path = Path(os.path.join(gettempdir(), "pwclatest.json"))
+            __path = Path(os.path.join(Path.cwd(), "pwclatest.json"))
 
             __lock = True
             if os.path.exists(__path):
                 shutil.copy(
-                    __path, Path(os.path.join(gettempdir(), "pwclatest_old.json"))
+                    __path, Path(os.path.join(Path.cwd(), "pwclatest_old.json"))
                 )
 
             data = {"papers": []}
@@ -110,7 +109,7 @@ class WebReader:
 
             __lock = False
 
-            __path = Path(os.path.join(gettempdir(), "pwcids.json"))
+            __path = Path(os.path.join(Path.cwd(), "pwcids.json"))
 
             with open(__path, "w", encoding="utf-8") as file:
                 file.writelines([id + "\n" for id in original + latest])

commit 1deb3f8c68f93f3d3d8dd94688ba98fe3b7ef11b

fix: Fix Path broken in 86ee955737d7d97b141ea20a2f57120e1f6afcd4

src/pwcode/utils/ConfigReader.py
@@ -26,7 +26,7 @@ class ConfigReader:
 
     def __init__(self):
         self.papers = 0
-        self.__path = '.pwconfig'
+        self.__path = Path(os.path.join(Path.cwd(), ".pwconfig"))
 
     def __exists(self):
         __path = self.__path